47a622ec28fe272e94d41f831cc8b26ab14251c8
Antony Padayattil <antonywired@gmail.com>
2018-08-01 18:28:52 +0530

    search query with handling for missing author and image

--- src/Book.js
+++ src/Book.js
@@ -1,24 +1,31 @@
 import React from 'react'
+import _ from 'lodash'
 
-const Book = props => (
-  <li>
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${props.book.imageLinks.thumbnail}")` }}></div>
-        <div className="book-shelf-changer">
-          <select value={props.book.shelf} onChange={e => props.moveBookTo(e.target.value, props.book)}>
-            <option value="move" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+const Book = props => {
+  const converImageUrl = _.get(props.book, 'imageLinks.thumbnail', false);
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          {converImageUrl ?
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${converImageUrl}")` }}></div>
+            : <div className="book-cover" style={{ width: 128, height: 193 }}></div>
+          }
+          <div className="book-shelf-changer">
+            <select value={props.book.shelf} onChange={e => props.moveBookTo(e.target.value, props.book)}>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
         </div>
+        <div className="book-title">{props.book.title}</div>
+        <div className="book-authors">{(props.book.authors || []).join(', ')}</div>
       </div>
-      <div className="book-title">{props.book.title}</div>
-      <div className="book-authors">{props.book.authors}</div>
-    </div>
-  </li>
-);
+    </li>
+  );
+}
 
 export default Book;
